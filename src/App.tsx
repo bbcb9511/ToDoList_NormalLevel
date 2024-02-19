5f842200541d5373d03b42b3bd4a8b4748410a81
murayama <murayama.daishi@itfllc.co.jp>
2024-02-19 11:06:53 +0900

    バックアップのため一旦コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Box, Button, ChakraProvider, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, List, ListItem, Select } from '@chakra-ui/react';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 function App() {
 
   const [todoList, setTodoList] = useState<Inputs[]>([]);
+  const [filterdTodoList, setFilterdTodoList] = useState<Inputs[]>([]);
   const [nextTodoId, setNextTodoId] = useState<number>(0);
+  const [filterValue, setFilterValue] = useState<string>('すべて');
+  const {register, handleSubmit, reset, formState:{errors}} = useForm<Inputs>();
 
   type Inputs = {
     id: number;
@@ -15,24 +18,54 @@ function App() {
     detail: string;
   }
 
-  const {register, handleSubmit, reset, control, formState:{errors}} = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data)
+  const onSubmit = (data: Inputs) => {
     setTodoList([...todoList, {
       id: nextTodoId,
       inputTodoName: data.inputTodoName,
       status: data.status,
       detail: data.detail
-      }])
+    }])
+
     reset()
     setNextTodoId(val => val + 1)
   }
 
-  const HtmlTodoList = todoList.map(todo => {
+  const onChangeFilter = (e:  React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterValue(e.target.value)
+    const targetTodoList = todoList.filter(todo => {
+      return(
+        e.target.value === 'すべて' ? true : todo.status === e.target.value
+      )
+    })
+    setFilterdTodoList(targetTodoList)
+  }
+
+  const onClickDelete = (deleteTargetId: number) => {
+    setTodoList(todoList.filter(todo => todo.id !== deleteTargetId))
+  }
+
+  useEffect(() => {
+    const targetTodoList = todoList.filter(todo => {
+      return(
+        filterValue === 'すべて' ? true : todo.status === filterValue
+      )
+    })
+    setFilterdTodoList(targetTodoList)
+  }, [todoList])
+
+  const HtmlTodoList = filterdTodoList.map(todo => {
     return(
       <ListItem>
-        ●{todo.inputTodoName}
+        <Flex textAlign={'center'} alignItems={'center'} my={'10px'}>
+          ●{todo.inputTodoName}
+          <Button 
+            border={'1px solid'} 
+            borderColor={'gray.400'} 
+            height={'30px'} 
+            mx={'20px'}
+            onClick={() => onClickDelete(todo.id)}
+          >削除</Button>
+        </Flex>
         <List>
           <ListItem pl={'20px'}>【ステータス】{todo.status}</ListItem>
           <ListItem pl={'20px'}>【詳細】{todo.detail}</ListItem>
@@ -40,8 +73,19 @@ function App() {
       </ListItem>
     )
   })
-  
 
+  // const HtmlOldTodoList = todoList.filter(todo => todo.status === '完了').map(todo => {
+  //   return(
+  //     <ListItem>
+  //       ●{todo.inputTodoName}
+  //       <List>
+  //         <ListItem pl={'20px'}>【ステータス】{todo.status}</ListItem>
+  //         <ListItem pl={'20px'}>【詳細】{todo.detail}</ListItem>
+  //       </List>
+  //     </ListItem>
+  //   )
+  // })
+  
 
   return (
     <ChakraProvider>
@@ -72,7 +116,7 @@ function App() {
           </FormControl>
 
           <FormControl isInvalid={!!errors.detail}>
-            <FormLabel>ToDo名</FormLabel>
+            <FormLabel>詳細</FormLabel>
             <Input 
               type='text' 
               placeholder='ToDoの詳細を入力してください' 
@@ -81,21 +125,52 @@ function App() {
             <Box color={'red'}>{!!errors.detail && <p>入力してください</p>}</Box>
           </FormControl>
 
-          <Button type='submit' m={'10px'} >登録</Button>
+          <Button type='submit' m={'10px'} border={'1px solid'} borderColor={'gray.400'}>登録</Button>
 
         </Box>
       </form>
 
+
       <Box m='30px' px={'40px'} py={'10px'} rounded='lg' bg='blue.50' >
         <Box textAlign='center'>
           <Heading as='h2' fontWeight='bold' fontSize={'25px'}>
-            未完了のToDo
+            ToDo一覧
           </Heading>
         </Box>
+
+        <Flex textAlign={'center'} alignItems={'center'} mb={'20px'}>
+          <FormLabel w={'100px'}>フィルター：</FormLabel>
+          <Select 
+            w={'120px'}
+            defaultValue='すべて'
+            value={filterValue}
+            onChange={(e) => onChangeFilter(e)}
+          >
+            <option value='すべて'>すべて</option>
+            <option value='未着手'>未着手</option>
+            <option value='対応中'>対応中</option>
+            <option value='完了'>完了</option>
+          </Select>                    
+        </Flex>
+
         <List spacing={5}>
           {HtmlTodoList}
         </List>
       </Box>
+
+
+      {/* <Box m='30px' px={'40px'} py={'10px'} rounded='lg' bg='gray.200' >
+        <Box textAlign='center'>
+          <Heading as='h2' fontWeight='bold' fontSize={'25px'}>
+            完了のToDo
+          </Heading>
+        </Box>
+
+        <List spacing={5}>
+          {HtmlOldTodoList}
+        </List>
+      </Box> */}
+
     </ChakraProvider>
   );
 }
